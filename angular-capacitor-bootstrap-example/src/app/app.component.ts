0b6736abaa90eecac3994ee5a8ff8c4a4e78b29b
startupscheune <fabbos@outlook.de>
2023-06-23 18:55:51 +0200

    some changes

--- angular-capacitor-bootstrap-example/src/app/app.component.ts
+++ angular-capacitor-bootstrap-example/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-capacitor-bootstrap-example';
 
+  
+
   onButtonClick() 
   {
     console.log('DEBUG onButtonClick');
