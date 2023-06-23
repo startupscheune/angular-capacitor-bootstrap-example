8bc835813134e98a1419f9f971c146dabd40cf7d
startupscheune <fabbos@outlook.de>
2023-06-23 18:57:57 +0200

    added navbar

--- angular-capacitor-bootstrap-example/src/app/app.module.ts
+++ angular-capacitor-bootstrap-example/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
