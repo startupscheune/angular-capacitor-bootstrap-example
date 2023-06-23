0b04a9c544129885e85ed1ba0b2e9072c3b7bf86
startupscheune <fabbos@outlook.de>
2023-06-23 18:04:12 +0200

    added jumbotron example

--- angular-capacitor-bootstrap-example/src/app/app.component.ts
+++ angular-capacitor-bootstrap-example/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-capacitor-bootstrap-example';
+
+  onButtonClick() 
+  {
+    console.log('DEBUG onButtonClick');
+  }
 }
